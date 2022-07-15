41b1a1d67735806e146aa4c19510b3840a3f8313
kibroman <you@example.comkibromanx@gmail.com>
2022-07-14 20:00:50 -0700

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,12 @@
-import './Home.css'
+import "./Home.css";
 import eat from "../assets/images/eat.gif";
 
 const Home = () => {
-    return (
-        <div>
-
-        <img src={eat} alt="van eat" />
-
-        </div>
-    )
-}
-
+  return (
+    <div>
+      <img src={eat} alt="van eat" />
+    </div>
+  );
+};
 
 export default Home;
